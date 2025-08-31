79495349198f96236c1cc9a5207f75b142f1ffc0
kanateetime <dondenden86@gmail.com>
2025-09-01 08:22:56 +0900

    fix//main_index.js: Add comment out
    fix index.html: fix name-main_index.html

---
+++ src/js/main_index.js
@@ -0,0 +1,8 @@
+//ページを飛ばすシステム
+/*document.getElementById("teacher").addEventListener("click",async()=>{
+    window.location.href = `https://dondenden.github.io/kanadon-karuta/implement/teach_index`
+});
+
+document.getElementById("student").addEventListener("click", async()=> {
+    window.location.href = 'https://dondenden.github.io/kanadon-karuta/implement/student_index'
+});*/

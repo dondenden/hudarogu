fed92c59d069d1f5af537612f580976640225918
kanateetime <dondenden86@gmail.com>
2025-09-03 00:17:33 +0900

    変更

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("student").addEventListener("click",async()=>{
+    window.location.href = `https://dondenden.github.io/kanadon-karuta/implement/teach_index`
+});
+
+document.getElementById("teacher").addEventListener("click", async()=> {
+    window.location.href = 'https://dondenden.github.io/kanadon-karuta/implement/student_index'
+});

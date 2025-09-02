c4f0f17c7a5f10952779ba4e1b5d123fb883cc5e
kanateetime <dondenden86@gmail.com>
2025-09-02 16:23:54 +0900

    all delite

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-//ページを飛ばすシステム
-/*document.getElementById("teacher").addEventListener("click",async()=>{
-    window.location.href = `https://dondenden.github.io/kanadon-karuta/implement/teach_index`
-});
-
-document.getElementById("student").addEventListener("click", async()=> {
-    window.location.href = 'https://dondenden.github.io/kanadon-karuta/implement/student_index'
-});*/

55b08e7bf792cabc1375b6fbb6feb00c1f7cc93d
kanateetime <dondenden86@gmail.com>
2025-09-05 01:17:21 +0900

    変更

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-//追加しただけ
+document.getElementById("teacher").addEventListener("click",async()=>{
+    window.location.href = `https://dondenden.github.io/hudarogu/src/teacher_hub`
+});
+
+/*document.getElementById("student").addEventListener("click", async()=> {
+    window.location.href = 'https://dondenden.github.io/kanadon-karuta/implement/student_index'
+});*/

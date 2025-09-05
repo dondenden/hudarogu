b99c846255ad0e4626ec5877c53100281de9964a
kanateetime <dondenden86@gmail.com>
2025-09-05 15:11:21 +0900

    変更

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ document.getElementById("teacher").addEventListener("click",async()=>{
     window.location.href = `https://dondenden.github.io/hudarogu/src/teacher_hub`
 });
 
-/*document.getElementById("student").addEventListener("click", async()=> {
-    window.location.href = 'https://dondenden.github.io/kanadon-karuta/implement/student_index'
-});*/
+document.getElementById("student").addEventListener("click", async()=> {
+    window.location.href = 'https://dondenden.github.io/hudarogu/src/student_hub'
+});

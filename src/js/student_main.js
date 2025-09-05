fbda9d4364284b412ed54c5bff2281ff081dc6a3
kanateetime <dondenden86@gmail.com>
2025-09-05 15:53:35 +0900

    変更

--- src/js/student_main.js
+++ src/js/student_main.js
@@ -1,5 +1,7 @@
 const currentParams = window.location.search; // 現在のURLパラメータを取得
 
+document.getElementById("name").textContent = `${schoolName}の${studentName}さんのメインフォーム`;
+
 document.getElementById("record").addEventListener("click", () => {
     // teacher_hub にパラメータを引き継ぐ
     window.location.href = `https://dondenden.github.io/hudarogu/src/student_matche${currentParams}`;

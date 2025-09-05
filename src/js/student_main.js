aac0a6f2c07708f5226e58cf09bc06b420a5f214
kanateetime <dondenden86@gmail.com>
2025-09-05 15:50:25 +0900

    変更

--- src/js/student_main.js
+++ src/js/student_main.js
@@ -1,11 +1,11 @@
 const currentParams = window.location.search; // 現在のURLパラメータを取得
 
-document.getElementById("teacher").addEventListener("click", () => {
+document.getElementById("record").addEventListener("click", () => {
     // teacher_hub にパラメータを引き継ぐ
-    window.location.href = `https://dondenden.github.io/hudarogu/src/teacher_hub${currentParams}`;
+    window.location.href = `https://dondenden.github.io/hudarogu/src/student_matche${currentParams}`;
 });
 
-document.getElementById("student").addEventListener("click", () => {
+document.getElementById("watch").addEventListener("click", () => {
     // student_hub にパラメータを引き継ぐ
     window.location.href = `https://dondenden.github.io/hudarogu/src/student_hub${currentParams}`;
 });

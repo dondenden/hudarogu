22d9b34ab9da3270804c40a0a7cd6bb11eccf54c
kanateetime <dondenden86@gmail.com>
2025-09-06 02:31:17 +0900

    変更

---
+++ src/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'hudarogu-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/hudarogu/index.html',
+  '/hudarogu/manifest.json',
+  '/hudarogu/css/index.css',
+  '/hudarogu/js/index.js',
+  '/hudarogu/icons/icon-192.png',
+  '/hudarogu/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
